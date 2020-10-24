2d0d1e077b956af354eb207d4acfb563f3a46e96
Naokazu Shimabukuro <shimabukuro@9qu.jp>
2020-10-24 15:02:08 +0900

    chore: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,17 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json'
+import eslint from '@rbnlffl/rollup-plugin-eslint';
+import pkg from './package.json';
+
+const banner = `
+/*!
+ * Asphalt.js
+ * Copyright (c) ${new Date().getFullYear()} ${pkg.author}
+ *
+ * Released under the MIT license
+ */
+`;
 
 const config = [
   {
@@ -10,17 +20,19 @@ const config = [
     output: {
       file: 'dist/index.js',
       format: 'umd',
-      name: pkg.name
+      banner
     },
-    external: [
-      ...Object.keys(pkg.peerDependencies || {})
-    ],
+    external: [...Object.keys(pkg.peerDependencies || {})],
     plugins: [
+      eslint({
+        fix: true
+      }),
       nodeResolve(),
       commonjs({
         include: 'node_modules/**'
       }),
       babel({
+        babelHelpers: 'bundled',
         exclude: 'node_modules/**'
       }),
       terser()
